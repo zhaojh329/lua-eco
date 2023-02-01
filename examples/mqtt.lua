792617a5ed84efc0c5d1dfb12cfb0782044c4dc5
Jianhui Zhao <zhaojh329@gmail.com>
2023-02-01 22:45:57 +0800

    mqtt: support mqtt
    depend on lua-mosquitto
    
    https://github.com/flukso/lua-mosquitto
    
    Signed-off-by: Jianhui Zhao <zhaojh329@gmail.com>

--- examples/mqtt.lua
+++ examples/mqtt.lua
@@ -1,111 +1,44 @@
 #!/usr/bin/env eco
 
--- https://github.com/flukso/lua-mosquitto
+local mqtt = require 'eco.mqtt'
 
-local socket = require 'eco.socket'
-local mosq  = require 'mosquitto'
-local time = require 'eco.time'
-local sys = require 'eco.sys'
+local con = mqtt.new('eco-' .. os.time())
 
-local MOSQ_ERR_NO_CONN = 4
-local MOSQ_ERR_CONN_LOST = 7
-
-local MOSQ_ID            = 'flukso'
-local MOSQ_CLEAN_SESSION = true
-local MOSQ_HOST          = '127.0.0.1'
-local MOSQ_PORT          = 1883
-local MOSQ_KEEPALIVE     = 300
-local MOSQ_MAX_READ      = 100 -- packets
-local MOSQ_MAX_WRITE     = 100 -- packets
-
-local done = false
-
-local mqtt = mosq.new(MOSQ_ID, MOSQ_CLEAN_SESSION)
-
-mqtt.ON_CONNECT = function(success, rc, str)
-    print('connected:', success, rc, str)
-
-    mqtt:subscribe('$SYS/#')
-    mqtt:subscribe('eco', 2)
+local ok, err = con:connect('localhost', 1883)
+if not ok then
+    print('connect fail:', err)
+    return
 end
 
-mqtt.ON_DISCONNECT = function(success, rc, str)
-    print('disconnected', success, rc, str)
-    if rc == MOSQ_ERR_CONN_LOST or rc == MOSQ_ERR_NO_CONN then
-        done = true
-    end
-end
+con:set_callback('ON_CONNECT', function(success, rc, str)
+    print('ON_CONNECT:', success, rc, str)
 
-mqtt.ON_PUBLISH = function(mid)
-    print('on publish: ', mid)
-end
+    con:subscribe('$SYS/#')
+    con:subscribe('eco', 2)
 
-mqtt.ON_MESSAGE = function(mid, topic, payload, qos, retain)
-    print('on message:', mid, topic, payload, qos, retain)
-end
-
--- mqtt.ON_SUBSCRIBE = function(...) print('SUBSCRIBE', ...) end
--- mqtt.ON_UNSUBSCRIBE = function(...) print('UNSUBSCRIBE', ...) end
--- mqtt.ON_LOG = function(...) print('LOG', ...) end
-
-local function wait_mqtt_connected(fd)
-    local w = eco.watcher(eco.IO, fd, eco.WRITE)
-    if not w:wait(3.0) then
-        return false, 'timeout'
-    end
-
-    local err = socket.getoption(fd, 'error')
-    if err ~= 0 then
-        return false, sys.strerror(err)
-    end
-
-    return true
-end
-
-local function connect_mqtt()
-    local ok, code, err = mqtt:connect_async(MOSQ_HOST, MOSQ_PORT, MOSQ_KEEPALIVE)
-    if not ok then
-        print('connect fail:', code, err)
-        os.exit(1)
-    end
-
-    local ok, err = wait_mqtt_connected(mqtt:socket())
-    if not ok then
-        print('connect fail:', err)
-        os.exit(1)
-    end
-end
-
-connect_mqtt()
-
-eco.run(function()
-    local fd = mqtt:socket()
-    local w = eco.watcher(eco.IO, fd)
-    while not done do
-        w:wait()
-        local _, err = mqtt:loop_read(MOSQ_MAX_READ)
-        if err == MOSQ_ERR_CONN_LOST then
-            break
-        end
-    end
+    con:publish("world", "hello")
 end)
 
-eco.run(function()
-    local fd = mqtt:socket()
-    local w = eco.watcher(eco.IO, fd, eco.WRITE)
-    while not done do
-        w:wait()
-        if mqtt:want_write() then
-            mqtt:loop_write(MOSQ_MAX_WRITE)
-        end
-    end
+con:set_callback('ON_DISCONNECT', function(success, rc, str)
+    print('ON_DISCONNECT:', success, rc, str)
 end)
 
-eco.run(function()
-    while not done do
-        time.sleep(3)
-        if not done then
-            mqtt:loop_misc()
-        end
-    end
+con:set_callback('ON_PUBLISH', function(mid)
+    print('ON_PUBLISH:', mid)
+end)
+
+con:set_callback('ON_MESSAGE', function(mid, topic, payload, qos, retain)
+    print('ON_MESSAGE:', mid, topic, payload, qos, retain)
+end)
+
+con:set_callback('ON_SUBSCRIBE', function(...)
+    print('ON_SUBSCRIBE:', ...)
+end)
+
+con:set_callback('ON_UNSUBSCRIBE', function(mid)
+    print('ON_UNSUBSCRIBE:', mid)
+end)
+
+con:set_callback('ON_LOG', function(level, str)
+    print('ON_LOG:', level, str)
 end)
---
+++ mqtt.lua
@@ -0,0 +1,237 @@
+--[[
+ * MIT License
+ *
+ * Copyright (c) 2022 Jianhui Zhao <zhaojh329@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+--]]
+
+-- https://github.com/flukso/lua-mosquitto
+
+local socket = require 'eco.socket'
+local mosq  = require 'mosquitto'
+local time = require 'eco.time'
+local sys = require 'eco.sys'
+local dns = require 'eco.dns'
+
+local M = {}
+
+local function read_loop(mt)
+    local done = mt.done
+    local con = mt.con
+    local w = mt.ior
+
+    while not done.v do
+        if not w:wait() then
+            break
+        end
+
+        if not con:loop_read(10) then
+            break
+        end
+    end
+end
+
+local function write_loop(mt)
+    local done = mt.done
+    local con = mt.con
+    local w = mt.iow
+
+    while not done.v do
+        if not w:wait() then
+            break
+        end
+
+        if con:want_write() then
+            if not con:loop_write(10) then
+                break
+            end
+        end
+
+        time.sleep(0.1)
+    end
+
+    mt.ior:cancel()
+end
+
+local function check_keepalive_loop(mt)
+    local done = mt.done
+    local con = mt.con
+
+    while not done.v do
+        if not con:loop_misc() then
+            break
+        end
+        time.sleep(1)
+    end
+end
+
+local methods = {}
+
+function methods:destroy()
+    local mt = getmetatable(self)
+    mt.done.v = true
+    mt.ior:cancel()
+    mt.iow:cancel()
+    return mt.con:destroy()
+end
+
+function methods:disconnect()
+    local mt = getmetatable(self)
+    return mt.con:disconnect()
+end
+
+function methods:reinitialise(id, clean_session)
+    local mt = getmetatable(self)
+    return mt.con:reinitialise(id, clean_session)
+end
+
+function methods:will_set(topic, payload, qos, retain)
+    local mt = getmetatable(self)
+    return mt.con:will_set(topic, payload, qos, retain)
+end
+
+function methods:will_clear()
+    local mt = getmetatable(self)
+    return mt.con:will_clear()
+end
+
+function methods:login_set(username, password)
+    local mt = getmetatable(self)
+    return mt.con:login_set(username, password)
+end
+
+function methods:tls_insecure_set(insecure)
+    local mt = getmetatable(self)
+    return mt.con:tls_insecure_set(insecure)
+end
+
+function methods:tls_set(cafile, capath, certfile, keyfile)
+    local mt = getmetatable(self)
+    return mt.con:tls_set(cafile, capath, certfile, keyfile)
+end
+
+function methods:tls_psk_set(psk, identity, ciphers)
+    local mt = getmetatable(self)
+    return mt.con:tls_psk_set(psk, identity, ciphers)
+end
+
+function methods:tls_opts_set(cert_required, tls_version, ciphers)
+    local mt = getmetatable(self)
+    return mt.con:tls_psk_set(cert_required, tls_version, ciphers)
+end
+
+function methods:option(option, value)
+    local mt = getmetatable(self)
+    return mt.con:option(option, value)
+end
+
+function methods:publish(topic, payload, qos, retain)
+    local mt = getmetatable(self)
+    return mt.con:publish(topic, payload, qos, retain)
+end
+
+function methods:subscribe(topic, qos)
+    local mt = getmetatable(self)
+    return mt.con:subscribe(topic, qos)
+end
+
+function methods:unsubscribe(topic)
+    local mt = getmetatable(self)
+    return mt.con:unsubscribe(topic)
+end
+
+function methods:set_callback(typ, func)
+    local mt = getmetatable(self)
+    return mt.con:callback_set(typ, func)
+end
+
+local function wait_connected(mt, con)
+    local fd = con:socket()
+    local w = eco.watcher(eco.IO, fd, eco.WRITE)
+    if not w:wait(3.0) then
+        return false, 'timeout'
+    end
+
+    local err = socket.getoption(fd, 'error')
+    if err ~= 0 then
+        return false, sys.strerror(err)
+    end
+
+    mt.connected = true
+
+    mt.ior = eco.watcher(eco.IO, fd)
+    mt.iow = w
+    mt.done = { v = false }
+
+    eco.run(read_loop, mt)
+    eco.run(write_loop, mt)
+    eco.run(check_keepalive_loop, mt)
+
+    return true
+end
+
+function methods:connect(host, port, keepalive)
+    local mt = getmetatable(self)
+    local con = mt.con
+
+    local answers, err = dns.query(host or 'localhost')
+    if not answers then
+        return false, err
+    end
+
+    local ok, err
+
+    for _, a in ipairs(answers) do
+        if a.type == dns.TYPE_A or a.type == dns.TYPE_AAAA then
+            ok, _, err = con:connect_async(a.address, port, keepalive)
+            if ok then
+                ok, err = wait_connected(mt, con)
+                if ok then
+                    return true
+                end
+            end
+        end
+    end
+
+    return false, err
+end
+
+function methods:reconnect()
+    local mt = getmetatable(self)
+    local con = mt.con
+
+    local ok, _, err = con:reconnect_async()
+    if not ok then
+        return false, err
+    end
+
+    return wait_connected(mt, con)
+end
+
+function M.new(id, clean_session)
+    local con = mosq.new(id, clean_session)
+
+    return setmetatable({}, {
+        __index = methods,
+        con = con
+    })
+end
+
+return setmetatable(M, { __index = mosq })
