482181d2cbab1e30a606abd6c4a35db38beda90b
Jianhui Zhao <zhaojh329@gmail.com>
2023-06-19 22:15:39 +0800

    chore: improve code
    Signed-off-by: Jianhui Zhao <zhaojh329@gmail.com>

--- examples/crypto/sha1.lua
+++ examples/crypto/sha1.lua
@@ -4,10 +4,13 @@ local sha1 = require 'eco.crypto.sha1'
 local hex = require 'eco.encoding.hex'
 
 local ctx = sha1.new()
-ctx:update('1234')
+
+ctx:update('12')
+ctx:update('34')
 
 local hash = ctx:final()
 print(hex.encode(hash))
+
 
 hash = sha1.sum('1234')
 print(hex.encode(hash))
