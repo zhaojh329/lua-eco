99045ca18fcc22d14bceb79ddafba1a5bc719f67
Jianhui Zhao <zhaojh329@gmail.com>
2023-07-30 20:59:24 +0800

    peat(binary): perfect example for binary
    Signed-off-by: Jianhui Zhao <zhaojh329@gmail.com>

--- examples/binary.lua
+++ examples/binary.lua
@@ -10,3 +10,12 @@ print(string.format('0x%x', binary.read_u16(data)))
 print(string.format('0x%x', binary.read_u16le(data)))
 
 print(string.format('0x%x', binary.read_u16be(data)))
+
+-- verify the returned value is valid
+local offset = 100
+local val, ok = binary.read_u16(data, offset)
+if not ok then
+    print('read fail')
+else
+    print('read value:', val)
+end
