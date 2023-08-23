28caa65c830b5a7ff1dcf089f82bcb8bbd7109eb
Jianhui Zhao <zhaojh329@gmail.com>
2023-08-23 10:19:21 +0000

    feat(example): perfect dns.lua
    Signed-off-by: Jianhui Zhao <zhaojh329@gmail.com>

--- examples/network/dns.lua
+++ examples/network/dns.lua
@@ -3,7 +3,19 @@
 local dns = require 'eco.dns'
 
 local answers, err = dns.query('bing.com')
+if not answers then
+    print('query fail:', err)
+    return
+end
 
+for _, a in ipairs(answers) do
+    for k, v in pairs(a) do
+        print(k, v)
+    end
+    print()
+end
+
+answers, err = dns.query('bing.com', { type = dns.TYPE_AAAA })
 if not answers then
     print('query fail:', err)
     return
