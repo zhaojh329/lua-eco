99c95d07f3051c60094d9c9b6d9c46ee4a2669ce
Jianhui Zhao <zhaojh329@gmail.com>
2023-10-08 14:26:43 +0800

    perf(nl): reduce memory usage
    Signed-off-by: Jianhui Zhao <zhaojh329@gmail.com>

--- nl.lua
+++ nl.lua
@@ -9,9 +9,7 @@ local M = {}
 local nl_methods = {}
 
 function nl_methods:bind(groups, pid)
-    local mt = getmetatable(self)
-
-    local ok, err = mt.sock:bind(groups, pid)
+    local ok, err = self.sock:bind(groups, pid)
     if not ok then
         return nil, err
     end
@@ -20,24 +18,19 @@ function nl_methods:bind(groups, pid)
 end
 
 function nl_methods:add_membership(group)
-    local mt = getmetatable(self)
-    return mt.sock:setoption('netlink_add_membership', group)
+    return self.sock:setoption('netlink_add_membership', group)
 end
 
 function nl_methods:drop_membership(group)
-    local mt = getmetatable(self)
-    return mt.sock:setoption('netlink_drop_membership', group)
+    return self.sock:setoption('netlink_drop_membership', group)
 end
 
 function nl_methods:send(msg)
-    local mt = getmetatable(self)
-    return mt.sock:sendto(msg:binary())
+    return self.sock:sendto(msg:binary())
 end
 
 function nl_methods:recv(n, timeout)
-    local mt = getmetatable(self)
-
-    local data, addr = mt.sock:recvfrom(n or 8192, timeout)
+    local data, addr = self.sock:recvfrom(n or 8192, timeout)
     if not data then
         return nil, addr
     end
@@ -46,9 +39,10 @@ function nl_methods:recv(n, timeout)
 end
 
 function nl_methods:close()
-    local mt = getmetatable(self)
-    mt.sock:close()
+    self.sock:close()
 end
+
+local metatable = { __index = nl_methods }
 
 function M.open(protocol)
     local sock, err = socket.netlink(protocol)
@@ -56,10 +50,7 @@ function M.open(protocol)
         return nil, 'create netlink socket fail: ' .. err
     end
 
-    return setmetatable({}, {
-        sock = sock,
-        __index = nl_methods,
-    })
+    return setmetatable({ sock = sock }, metatable)
 end
 
 return setmetatable(M, { __index = nl })
