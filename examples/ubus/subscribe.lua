19dc705aba05ab3e7248048c7ab0aeca5ce0009a
Jianhui Zhao <zhaojh329@gmail.com>
2025-01-22 23:24:49 +0800

    feat(ubus): add support publish/subscriber.
    Signed-off-by: Jianhui Zhao <zhaojh329@gmail.com>

---
+++ examples/ubus/subscribe.lua
@@ -0,0 +1,37 @@
+#!/usr/bin/env eco
+
+local ubus = require 'eco.ubus'
+local time = require 'eco.time'
+
+eco.run(function()
+    local con, err = ubus.connect()
+    if not con then
+        error(err)
+    end
+
+    local obj = con:add('eco', {})
+
+    while true do
+        time.sleep(1)
+        local ts = time.now()
+        print('notify...', ts)
+        con:notify(obj, 'time', { ts = ts })
+    end
+end)
+
+eco.run(function()
+    local con, err = ubus.connect()
+    if not con then
+        error(err)
+    end
+
+    con:subscribe('eco', function(method, msg)
+        if method == 'time' then
+            print('recv:', msg.ts)
+        end
+    end)
+
+    while true do
+        time.sleep(1000)
+    end
+end)
