48d782ff80aba0abab376428cddb9caf03a888fe
Jianhui Zhao <zhaojh329@gmail.com>
2025-05-05 21:52:12 +0800

    perf: sleep timer for each coroutine are allocated as needed.
    Signed-off-by: Jianhui Zhao <zhaojh329@gmail.com>

--- time.c
+++ time.c
@@ -3,6 +3,8 @@
  * Author: Jianhui Zhao <zhaojh329@gmail.com>
  */
 
+#include <stdlib.h>
+
 #include "eco.h"
 
 /* returns the Unix time, the number of seconds elapsed since January 1, 1970 UTC */
@@ -14,6 +16,14 @@ static int lua_ev_now(lua_State *L)
     lua_pushnumber(L, ev_now(ctx->loop));
 
     return 1;
+}
+
+static void eco_sleep_cb(struct ev_loop *loop, struct ev_timer *w, int revents)
+{
+    lua_State *co = w->data;
+    struct eco_context *ctx = eco_get_context(co);
+
+    eco_resume(ctx->L, co, 0);
 }
 
 /*
@@ -34,8 +44,22 @@ static int lua_sleep(lua_State *L)
     lua_rawget(L, -2); /* ctx_env, tmr_ptr */
 
     tmr = (struct ev_timer *)lua_topointer(L, -1);
+    if (!tmr) {
+        tmr = calloc(1, sizeof(struct ev_timer));
+        if (!tmr)
+            return luaL_error(L, "no mem");
 
-    lua_pop(L, 2);
+        ev_init(tmr, eco_sleep_cb);
+        tmr->data = L;
+
+        lua_pop(L, 1);
+        lua_pushthread(L);
+        lua_pushlightuserdata(L, tmr);
+        lua_rawset(L, -3);  /* ctx_env[co] = tmr_ptr */
+        lua_pop(L, 1);
+    } else {
+        lua_pop(L, 2);
+    }
 
     ev_timer_set(tmr, delay, 0.0);
     ev_timer_start(ctx->loop, tmr);
